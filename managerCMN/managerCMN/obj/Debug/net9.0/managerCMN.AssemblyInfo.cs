15c6124cd4a0c63dca62e6e7adfc59141613522a
hyhoncute <vodaohuyhoang@gmail.com>
2026-03-30 12:02:47 +0700

    update bk and re DB + FILE

--- managerCMN/managerCMN/obj/Debug/net9.0/managerCMN.AssemblyInfo.cs
+++ managerCMN/managerCMN/obj/Debug/net9.0/managerCMN.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("managerCMN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af82d443582539b0367f8e10f1813f556df6b885")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e19fbb69d20b927aea6d4c6144945968adee740d")]
 [assembly: System.Reflection.AssemblyProductAttribute("managerCMN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("managerCMN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

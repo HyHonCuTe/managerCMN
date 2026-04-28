caf6728419b749ae5df4d7518c143cd8f748fb48
hyhoncute <vodaohuyhoang@gmail.com>
2026-04-28 13:13:05 +0700

    fix: đếm nhắc hẹn dl cho đúng

--- managerCMN/managerCMN/Services/Implementations/TaskDeadlineReminderService.cs
+++ managerCMN/managerCMN/Services/Implementations/TaskDeadlineReminderService.cs
@@ -109,8 +109,8 @@ public class TaskDeadlineReminderService : BackgroundService
                 continue;
 
             var projectName = projectNames.TryGetValue(task.ProjectId, out var pn) ? pn : "Dự án";
-            // Treat task deadline as end of due date (midnight of next day)
-            var deadline = task.DueDate!.Value.Date.AddDays(1);
+            // Task deadline is the date itself (midnight start of due date)
+            var deadline = task.DueDate!.Value.Date;
             var remaining = deadline - now;
             var timeLeft = FormatTimeLeft(remaining);
             var dueDateStr = task.DueDate.Value.ToString("dd/MM/yyyy");
